52f1fb171bfdfd5f1cf66ffb4cb3873372d03789
nastcollege <nastcollegegit@gmail.com>
2024-12-04 11:45:11 +0545

    Group 2 lab work

---
+++ app/src/main/java/np/edu/nast/myappg2/DashboardActivity.java
@@ -0,0 +1,13 @@
+package np.edu.nast.myappg2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class DashboardActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/np/edu/nast/myappg2/LoginActivity.java
@@ -0,0 +1,43 @@
+package np.edu.nast.myappg2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class LoginActivity extends Activity {
+    private Button btnLogin;
+    private EditText etUsername;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin=findViewById(R.id.btnLogin);
+        etUsername=findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //event handling code goes here
+                if(etUsername.getText().toString().equals("NAST") && etPassword.getText().toString().equals("BE7th")){
+
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(),"You enter invalid credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}

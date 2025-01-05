9514e119ac43658b0c8ac9afe19788436b63ed21
nastcollege <nastcollegegit@gmail.com>
2025-01-05 11:54:09 +0545

    user registration page added

--- app/src/main/java/np/edu/nast/myappg2/LoginActivity.java
+++ app/src/main/java/np/edu/nast/myappg2/LoginActivity.java
@@ -15,30 +15,41 @@ public class LoginActivity extends Activity {
     private EditText etUsername;
     private EditText etPassword;
 
+    private Button btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin=findViewById(R.id.btnLogin);
-        etUsername=findViewById(R.id.etUsername);
+        btnLogin = findViewById(R.id.btnLogin);
+        etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+
+        btnSignup = findViewById(R.id.btnSignup);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //event handling code goes here
-                if(etUsername.getText().toString().equals("NAST") && etPassword.getText().toString().equals("BE7th")){
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                if (etUsername.getText().toString().equals("NAST") && etPassword.getText().toString().equals("BE7th")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     //intent to pass data from one activity to another activity
                     intent.putExtra("user", etUsername.getText().toString());
                     startActivity(intent);
                     finish();
-                }else{
-                    Toast.makeText(getApplicationContext(),"You enter invalid credentials",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "You enter invalid credentials", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

bec10572e56bbaf9a72489db16cac7b759a239a3
nastcollege <nastcollegegit@gmail.com>
2025-01-05 12:08:19 +0545

    Logging

--- app/src/main/java/np/edu/nast/myappg2/SignupActivity.java
+++ app/src/main/java/np/edu/nast/myappg2/SignupActivity.java
@@ -1,6 +1,10 @@
 package np.edu.nast.myappg2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText etFirst, etLast, etEmail, etPass, etConfirm;
+    private Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        etFirst = findViewById(R.id.etFirstName);
+        etLast = findViewById(R.id.etLastName);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPassword);
+        etConfirm = findViewById(R.id.etConfirm);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnRegister);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("KEY",etFirst.getText().toString());
+                Log.e("KEY",etLast.getText().toString());
+                Log.d("KEY",etPass.getText().toString());
+                Log.w("KEY","Warning");
+            }
+        });
 
     }
 }
